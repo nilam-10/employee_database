04bae32655da9272e56e0ac4a98ce39d0df733eb
Kanak Meshram <meshramkp@rknec.edu>
2025-04-05 10:57:18 +0530

    Updated model

--- employee_crud/employeeapp/models.py
+++ employee_crud/employeeapp/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from decimal import Decimal
+from datetime import date
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, default='example@example.com')
+
     phone_number = models.CharField(max_length=20, blank=True, null=True)
+
     gender = models.CharField(max_length=20, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
 
@@ -16,14 +19,16 @@ class Employee(models.Model):
     department = models.CharField(max_length=100, blank=True, null=True)
 
     projects_completed = models.PositiveIntegerField(default=0, verbose_name="Projects Completed This Year")
-    monthly_salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Monthly Salary")
-    date_joined = models.DateField(verbose_name="Date of Joining")
+    monthly_salary = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, verbose_name="Monthly Salary")
 
+
+    date_joined = models.DateField(verbose_name="Date of Joining", blank=True, null=True)
 
     @property
     def term_in_company(self):
-        from datetime import date
-        return date.today().year - self.date_joined.year
+        if self.date_joined:
+            return date.today().year - self.date_joined.year
+        return 0
 
     def __str__(self):
         return f"{self.name} {self.surname}"
