665be3a9dc46b3d0b11c56d8acdbc018eee7e357
Kanak Meshram <meshramkp@rknec.edu>
2025-04-05 10:22:10 +0530

    Updated model

--- employee_crud/employeeapp/models.py
+++ employee_crud/employeeapp/models.py
@@ -2,20 +2,14 @@ from django.db import models
 from decimal import Decimal
 
 class Employee(models.Model):
-    GENDER_CHOICES = [
-        ('Male', 'Male'),
-        ('Female', 'Female'),
-        ('Other', 'Other'),
-    ]
-
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     bank_account_no = models.CharField(max_length=30, verbose_name="Bank Account Number")
-    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
-    address = models.TextField()
-    projects_done_in_year = models.IntegerField()
-    salary = models.DecimalField(max_digits=10, decimal_places=2)
-    date_joined = models.DateField()
+    gender = models.CharField(max_length=20) 
+    address = models.TextField(verbose_name="Residential Address")
+    projects_completed = models.IntegerField(verbose_name="Projects Completed This Year")
+    monthly_salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Monthly Salary")
+    date_joined = models.DateField(verbose_name="Date of Joining")
 
     @property
     def term_in_company(self):
