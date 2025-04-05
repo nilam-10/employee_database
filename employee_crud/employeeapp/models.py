839b36fefdd2264e26e2f85ebdad2a26030a9614
Kanak Meshram <meshramkp@rknec.edu>
2025-04-05 10:28:27 +0530

    Updated input fields of model

--- employee_crud/employeeapp/models.py
+++ employee_crud/employeeapp/models.py
@@ -4,12 +4,22 @@ from decimal import Decimal
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(max_length=20, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+
     bank_account_no = models.CharField(max_length=30, verbose_name="Bank Account Number")
-    gender = models.CharField(max_length=20) 
     address = models.TextField(verbose_name="Residential Address")
-    projects_completed = models.IntegerField(verbose_name="Projects Completed This Year")
+
+    position = models.CharField(max_length=100, blank=True, null=True)
+    department = models.CharField(max_length=100, blank=True, null=True)
+
+    projects_completed = models.PositiveIntegerField(default=0, verbose_name="Projects Completed This Year")
     monthly_salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Monthly Salary")
     date_joined = models.DateField(verbose_name="Date of Joining")
+
+    is_active = models.BooleanField(default=True)
 
     @property
     def term_in_company(self):
