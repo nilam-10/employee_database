59d8cc0baa3eac79a14298626b9919b1b908f3eb
nilam-10 <bhojwaninv@rknec.edu>
2025-04-11 17:34:19 +0530

    Update views.py

--- employee_crud/employeeapp/views.py
+++ employee_crud/employeeapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Employee
-from .forms import EmployeeForm
 
 def employee_list(request):
     employees = Employee.objects.all()
@@ -8,21 +7,22 @@ def employee_list(request):
 
 def employee_create(request):
     if request.method == 'POST':
-        form = EmployeeForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('employee_list')
-    else:
-        form = EmployeeForm()
-    return render(request, 'employeeapp/form.html', {'form': form})
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        department = request.POST.get('department')
+        Employee.objects.create(name=name, email=email, department=department)
+        return redirect('employee_list')
+    return render(request, 'employeeapp/form.html')
 
 def employee_update(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
-    form = EmployeeForm(request.POST or None, instance=employee)
-    if form.is_valid():
-        form.save()
+    if request.method == 'POST':
+        employee.name = request.POST.get('name')
+        employee.email = request.POST.get('email')
+        employee.department = request.POST.get('department')
+        employee.save()
         return redirect('employee_list')
-    return render(request, 'employeeapp/form.html', {'form': form})
+    return render(request, 'employeeapp/form.html', {'employee': employee})
 
 def employee_delete(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
